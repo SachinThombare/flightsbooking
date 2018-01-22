e859be68d1154bfbddeea6b8ebac9c60999c693a
Sachin Thombare <sachin.thombare5791@gmail.com>
2018-01-22 09:10:55 +0530

    Changed implicit wait time

--- src/test/java/com/expedia/flightsbooking/TestNG_TestSuite.java
+++ src/test/java/com/expedia/flightsbooking/TestNG_TestSuite.java
@@ -25,7 +25,7 @@ public class TestNG_TestSuite {
 
 		// Maximize the browser's window
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		PropertyConfigurator.configure("log4j.properties");
 		driver.get(baseUrl);
 	}
